911f3251e82d48d361a2f183175d994029e2b1b3
Duy Nguyen <duy.nguyencong@pycogroup.com>
2018-08-28 15:34:47 +0700

    feat(elements): adds modal

---
+++ elements/modal/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <slot>
+            Default Slot
+          </slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+@import './index.css';
+</style>

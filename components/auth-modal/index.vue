4ed61e2e1c41026ea601fe133cb0e8d21190d0ba
Duy Nguyen <duy.nguyencong@pycogroup.com>
2018-08-31 14:10:20 +0700

    feat(auth-modal): integrates with auth-form

--- components/auth-modal/index.vue
+++ components/auth-modal/index.vue
@@ -1,13 +1,17 @@
 <template>
   <Modal>
     <div class="container">
-      <h3 class="title">Please login to continue</h3>
-      <Tabs :options="{ useUrlFragment: false }">
+      <h3 class="title">Please login to continue.</h3>
+      <Tabs :options="{ useUrlFragment: false }" @changed="handleTabChange">
         <Tab id="auth/login" name="Login">
-          <p>This is Login Page</p>
+          <div class="form-container">
+            <AuthForm :isLogin="true" @submit="handleLogin"/>
+          </div>
         </Tab>
         <Tab id="auth/register" name="Register">
-          <p>This is Register Page</p>
+          <div class="form-container">
+            <AuthForm :isLogin="false" @submit="handleRegister"/>
+          </div>
         </Tab>
       </Tabs>
     </div>
@@ -16,10 +20,12 @@
 
 <script>
 import { Tabs, Tab } from 'vue-tabs-component';
+
+import AuthForm from '~/components/auth-form';
 import Modal from '~/elements/modal';
 
 export default {
-  components: { Modal, Tabs, Tab }
+  components: { Modal, Tabs, Tab, AuthForm }
 };
 </script>
 
